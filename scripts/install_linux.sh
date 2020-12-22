692286c83d734cbb6e8f564957ed8fc1051b8b0c
hundehausen <mail@grischadaum.de>
2020-12-22 01:10:06 +0100

    updated install scripts

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -6,7 +6,23 @@ else
     echo "Docker running.."
 fi
 
+if ! docker-compose version >/dev/null 2>&1; then
+    echo "Docker Compose does not seem to be running, run it first and retry"
+    exit 1
+else
+    echo "Docker Compose running.."
+fi
+
+if ! git version >/dev/null 2>&1; then
+    echo "git is not installed.. sudo apt install -y git"
+    exit 1
+fi
+
 mkdir -p ../projects
 cd ../projects
 git clone https://github.com/hundehausen/tipxmr.git
 git clone https://github.com/hundehausen/tipxmr-backend.git
+cd tipxmr
+git checkout dockerize
+cd ../tipxmr-backend
+git checkout dockerize
